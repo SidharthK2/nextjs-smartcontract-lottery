abca4133e467c5d2498b2663813bcf26eccd437b
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-07 23:19:31 +0530

    add LotteryEntrance component

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
 
 export default function LotteryEntrance() {
-  // const {runContractFunction: enterRaffle} = useWeb3Contract({
-  //     abi:,
-  //     contractAddress:,
-  //     functionName:,
-  //     params: {},
-  //     msgValue:
-  // })
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const chainId = parseInt(chainIdHex);
+  const raffleAddress =
+    chainIdHex in contractAddresses ? contractAddresses[chainId][0] : null;
+  // const { runContractFunction: enterRaffle } = useWeb3Contract({
+  //   abi: abi,
+  //   contractAddress: raffleAddress,
+  //   functionName: "enterRaffle",
+  //   params: {},
+  //   // msgValue:
+  // });
 
-  return <div className="">Lottery Entrance</div>;
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      async function updateUI() {
+        const entranceFee = await getEntranceFee();
+        console.log(entranceFee);
+      }
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
+  return <div>Lottery Entrance</div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import ManualHeader from "../components/ManualHeader";
+import Header from "../components/Header";
 import LotteryEntrance from "../components/LotteryEntrance";
 
 export default function Home() {
@@ -12,7 +12,8 @@ export default function Home() {
         <meta name="description" content="Smart contract lottery" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ManualHeader />
+      <Header />
+      <LotteryEntrance />
     </div>
   );
 }
