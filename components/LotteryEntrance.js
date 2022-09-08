d597077e5ae22af22d4d829595c62fd2e9907b0a
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-08 19:10:24 +0530

    fix getEntranceFee function

---
+++ components/LotteryEntrance.js
@@ -0,0 +1,36 @@
+import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
+
+export default function LotteryEntrance() {
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const chainId = parseInt(chainIdHex);
+  const raffleAddress =
+    chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+  // const { runContractFunction: enterRaffle } = useWeb3Contract({
+  //   abi: abi,
+  //   contractAddress: raffleAddress,
+  //   functionName: "enterRaffle",
+  //   params: {},
+  //   msgValue: ,
+  // });
+
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      async function updateUI() {
+        const smth = await getEntranceFee();
+        console.log(smth);
+      }
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+  return <div className="">Hi from lottery entrance</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import LotteryEntrance from "../components/LotteryEntrance";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      Hello!
+      <LotteryEntrance />
     </div>
   );
 }
