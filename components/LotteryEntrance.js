fdf56f1b4f14e57da8c2e2ae8d72f5e0f3f12e69
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-12 19:28:02 +0530

    add enter raffle button

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -3,14 +3,17 @@ import { abi, contractAddresses } from "../constants";
 import { useMoralis } from "react-moralis";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
 
 export default function LotteryEntrance() {
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
   const chainId = parseInt(chainIdHex);
   const raffleAddress =
     chainId in contractAddresses ? contractAddresses[chainId][0] : null;
-
   const [entranceFee, setEntranceFee] = useState("0");
+
+  const dispatch = useNotification();
+
   const { runContractFunction: enterRaffle } = useWeb3Contract({
     abi: abi,
     contractAddress: raffleAddress,
@@ -30,15 +33,44 @@ export default function LotteryEntrance() {
     if (isWeb3Enabled) {
       async function updateUI() {
         const entranceFeeFromCall = (await getEntranceFee()).toString();
-        setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, "ether"));
+        setEntranceFee(entranceFeeFromCall, "ether");
       }
       updateUI();
     }
   }, [isWeb3Enabled]);
+
+  const handleSuccess = async (tx) => {
+    await tx.wait(1);
+    handleNewNotification(tx);
+  };
+  const handleNewNotification = () => {
+    dispatch({
+      type: "info",
+      message: "Transaction Complete!",
+      title: "Tx Notification",
+      position: "topR",
+      icon: "bell",
+    });
+  };
   return (
     <div className="">
       Hi from lottery entrance
-      <div>Entrance Fee: {entranceFee} ETH</div>
+      {raffleAddress ? (
+        <div>
+          <button
+            onClick={async () => {
+              await enterRaffle({
+                onSuccess: handleSuccess,
+                onError: (error) => console.log(error),
+              });
+            }}>
+            Enter Raffle
+          </button>
+          Entrance Fee: {ethers.utils.formatUnits(entranceFee)} ETH
+        </div>
+      ) : (
+        <div>No Raffle Address detected</div>
+      )}
     </div>
   );
 }
